4a15c4b82d5aa128778080fb3349f5c0ec3d4173
Antonio Mata <hildeamp@gmail.com>
2020-01-06 15:37:55 -0600

    :card_file_box: add database connection to app module
    add database connection to database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: 'postgres' as 'postgres',
+          port: configService.get('DB_PORT'),
+          host: configService.get('DB_HOST'),
+          database: configService.get('DB_NAME'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
+          synchronize: true,
+          entities: ['dist/**/*.entity{.ts|.js}'],
+        } as TypeOrmModuleOptions),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
