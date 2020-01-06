c1107ac5a2cdc5c71d4ba8b26a072157362292da
Antonio Mata <hildeamp@gmail.com>
2020-01-06 15:46:43 -0600

    :recycle: use TypeOrmConfigService
    use TypeOrmConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './typeorm-config.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) =>
-        ({
-          type: 'postgres' as 'postgres',
-          port: configService.get('DB_PORT'),
-          host: configService.get('DB_HOST'),
-          database: configService.get('DB_NAME'),
-          username: configService.get('DB_USER'),
-          password: configService.get('DB_PASS'),
-          synchronize: true,
-          entities: ['dist/**/*.entity{.ts|.js}'],
-        } as TypeOrmModuleOptions),
-      inject: [ConfigService],
+      useClass: TypeOrmConfigService,
     }),
   ],
   controllers: [AppController],
