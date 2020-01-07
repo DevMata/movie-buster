e0ee93fea539e01988e36bf38ff5cdc6cd779acd
Antonio Mata <hildeamp@gmail.com>
2020-01-07 10:48:40 -0600

    :card_file_box: add MovieEntity
    add MovieEntity

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+} from 'typeorm';
+import { Tag } from 'src/tags/entities/tag.entity';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn('uuid')
+  movieId: string;
+
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ length: 500 })
+  description: string;
+
+  @Column()
+  poster: string;
+
+  @Column()
+  trailer: string;
+
+  @Column()
+  rentPrince: number;
+
+  @Column()
+  salePrice: number;
+
+  @Column({ type: 'integer' })
+  stock: number;
+
+  @Column({ type: 'integer' })
+  likes: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  modifiedAt: Date;
+
+  @ManyToMany(
+    () => Tag,
+    tag => tag.movies,
+  )
+  tags: Tag[];
+}
