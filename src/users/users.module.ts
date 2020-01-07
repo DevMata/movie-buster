60d8f6004c5c7582f7e6162d9054fbc7a62d8612
Antonio Mata <hildeamp@gmail.com>
2020-01-07 15:44:26 -0600

    :recycle: export UsersService from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,5 +6,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
