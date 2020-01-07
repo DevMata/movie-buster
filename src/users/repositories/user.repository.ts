e835e01aa33f4039e82e2d26058512ff40694dc7
Antonio Mata <hildeamp@gmail.com>
2020-01-07 12:04:47 -0600

    :card_file_box: add UserRepository

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -1,5 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { ConflictException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,5 +11,13 @@ export class UserRepository extends Repository<User> {
 
   findUserByEmail(email: string): Promise<User> {
     return this.findOne({ email: email });
+  }
+
+  createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.findUserByEmail(createUserDto.email);
+    if (user)
+      throw new ConflictException('provided email is already registered');
+
+    return this.save(createUserDto);
   }
 }
