4338e428f5b613ae6c636b808ef6aedf77d3c50d
Antonio Mata <hildeamp@gmail.com>
2020-01-07 11:53:13 -0600

    :heavy_plus_sign: add class-validator and class-transformer

---
+++ src/users/repositories/user.repository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  findUserById(userId: string): Promise<User> {
+    return this.findOne(userId);
+  }
+
+  findUserByEmail(email: string): Promise<User> {
+    return this.findOne({ email: email });
+  }
+}
