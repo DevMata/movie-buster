5be735c9cb4112412b38923138c302a47e5d0401
Antonio Mata <hildeamp@gmail.com>
2020-01-07 15:42:11 -0600

    :recycle: import AuthenticationModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { TagsModule } from './tags/tags.module';
 import { RolesModule } from './roles/roles.module';
 import { MoviesModule } from './movies/movies.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { MoviesModule } from './movies/movies.module';
     TagsModule,
     RolesModule,
     MoviesModule,
+    AuthenticationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
