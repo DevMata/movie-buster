202fd3e71e1254d3bb9e1ae82714e1d9d2ef4ef2
Antonio Mata <hildeamp@gmail.com>
2020-01-08 15:53:53 -0600

    :recycle: import TagsModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieRepository } from './repositories/movie.repository';
+import { MoviesService } from './movies.service';
+import { MoviesController } from './movies.controller';
+import { TagsModule } from 'src/tags/tags.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieRepository])],
+  imports: [TypeOrmModule.forFeature([MovieRepository]), TagsModule],
+  providers: [MoviesService],
+  controllers: [MoviesController],
 })
 export class MoviesModule {}
