eb33572cdd214d1893b97efe312e8fab6702c061
Antonio Mata <hildeamp@gmail.com>
2020-01-08 11:37:50 -0600

    :sparkles: add getTags method

--- src/tags/repositories/tag.repository.ts
+++ src/tags/repositories/tag.repository.ts
@@ -4,6 +4,10 @@ import { CreateTagDto } from '../dto/create-tag.dto';
 
 @EntityRepository(Tag)
 export class TagRepository extends Repository<Tag> {
+  getTags(): Promise<Array<Tag>> {
+    return this.find();
+  }
+
   findTagById(tagId: string): Promise<Tag> {
     return this.findOne(tagId);
   }
