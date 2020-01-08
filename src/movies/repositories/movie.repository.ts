0598b315f87bc0d9ef1ec89c3f3d1d03dff4617d
Antonio Mata <hildeamp@gmail.com>
2020-01-08 15:52:53 -0600

    :card_file_box: refactor MovieRepository

--- src/movies/repositories/movie.repository.ts
+++ src/movies/repositories/movie.repository.ts
@@ -1,15 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Movie } from '../entities/movie.entity';
-import { CreateMovieDto } from '../dto/create-movie.dto';
-import { ConflictException } from '@nestjs/common';
-import { TagRepository } from 'src/tags/repositories/tag.repository';
 
 @EntityRepository(Movie)
 export class MovieRepository extends Repository<Movie> {
-  constructor(private readonly tagRepository: TagRepository) {
-    super();
-  }
-
   getMovies(): Promise<Array<Movie>> {
     return this.find({ select: ['movieId', 'title', 'description'] });
   }
@@ -20,17 +13,5 @@ export class MovieRepository extends Repository<Movie> {
 
   findMovieByTitle(title: string): Promise<Movie> {
     return this.findOne({ title: title });
-  }
-
-  async createMovie(createMovieDto: CreateMovieDto): Promise<Movie> {
-    const movie = await this.findMovieByTitle(createMovieDto.title);
-
-    if (movie)
-      throw new ConflictException('A movie with that title already exists');
-
-    return this.save({
-      ...createMovieDto,
-      tags: await this.tagRepository.createMultipleTags(createMovieDto.tags),
-    });
   }
 }
