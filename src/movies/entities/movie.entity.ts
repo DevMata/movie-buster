8feec81714fcb785115c8cdd0f8693828070af86
Antonio Mata <hildeamp@gmail.com>
2020-01-08 16:51:22 -0600

    :card_file_box: add like ManyToMany between Movies and Users

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -8,6 +8,7 @@ import {
   JoinTable,
 } from 'typeorm';
 import { Tag } from 'src/tags/entities/tag.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Entity()
 export class Movie {
@@ -44,7 +45,11 @@ export class Movie {
   @UpdateDateColumn()
   modifiedAt: Date;
 
-  @ManyToMany(() => Tag)
-  @JoinTable()
+  @ManyToMany(() => Tag, { eager: true })
+  @JoinTable({ name: 'movie_tag' })
   tags: Tag[];
+
+  @ManyToMany(() => User)
+  @JoinTable({ name: 'like' })
+  users: User[];
 }
