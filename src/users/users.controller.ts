4901ecaaa7319fe44116c54bf09dcf7146ec3482
Antonio Mata <hildeamp@gmail.com>
2020-01-08 18:06:41 -0600

    :sparkles: add Put route and updateUser method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UserIdDto } from './dto/user-id.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -21,5 +22,13 @@ export class UsersController {
   @Get(':userId')
   getUser(@Param() userIdDto: UserIdDto): Promise<User> {
     return this.usersService.findUserById(userIdDto.userId);
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param() userIdDto: UserIdDto,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateUser(userIdDto.userId, updateUserDto);
   }
 }
