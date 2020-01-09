94a205332534e1cfa5cd41e8c3aebb85aa6415f5
Antonio Mata <hildeamp@gmail.com>
2020-01-09 12:08:30 -0600

    :recycle: add UsersModule into MoviesModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -4,9 +4,14 @@ import { MovieRepository } from './repositories/movie.repository';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
 import { TagsModule } from 'src/tags/tags.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieRepository]), TagsModule],
+  imports: [
+    TypeOrmModule.forFeature([MovieRepository]),
+    TagsModule,
+    UsersModule,
+  ],
   providers: [MoviesService],
   controllers: [MoviesController],
 })
