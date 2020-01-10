e102582e1c7b800482add100b692fd5ae30f3a0a
Antonio Mata <hildeamp@gmail.com>
2020-01-10 03:11:57 -0600

    refactor imports

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -1,19 +1,12 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { RentsService } from './rents.service';
 import { RentsController } from './rents.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Rent } from './entities/rent.entity';
-import { UsersModule } from 'src/users/users.module';
 import { MoviesModule } from 'src/movies/movies.module';
-import { AuthenticationModule } from 'src/authentication/authentication.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Rent]),
-    forwardRef(() => UsersModule),
-    MoviesModule,
-    AuthenticationModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Rent]), MoviesModule],
   providers: [RentsService],
   controllers: [RentsController],
 })
