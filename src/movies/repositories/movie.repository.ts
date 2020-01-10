f759b1ab7ec19d807527541e7f9018013d0426d9
Antonio Mata <hildeamp@gmail.com>
2020-01-09 23:29:49 -0600

    :fire: remove updateMovie from MovieRepository

--- src/movies/repositories/movie.repository.ts
+++ src/movies/repositories/movie.repository.ts
@@ -1,7 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Movie } from '../entities/movie.entity';
-import { UpdateMovieDto } from '../dto/update-movie.dto';
-import { NotFoundException } from '@nestjs/common';
 
 @EntityRepository(Movie)
 export class MovieRepository extends Repository<Movie> {
@@ -17,15 +15,5 @@ export class MovieRepository extends Repository<Movie> {
 
   findMovieByTitle(title: string): Promise<Movie> {
     return this.findOne({ title: title });
-  }
-
-  async updateMovie(
-    movieId: string,
-    updateMovieDto: UpdateMovieDto,
-  ): Promise<Movie> {
-    const movie = await this.findOne(movieId);
-    if (!movie) throw new NotFoundException('movie not found');
-
-    return this.save({ ...movie, ...updateMovieDto });
   }
 }
