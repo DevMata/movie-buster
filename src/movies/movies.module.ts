9c0e939f7e0a63828a351e55ff3ee2314d0d7475
Antonio Mata <hildeamp@gmail.com>
2020-01-10 03:11:33 -0600

    exports UsersModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -5,17 +5,15 @@ import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
 import { TagsModule } from 'src/tags/tags.module';
 import { UsersModule } from 'src/users/users.module';
-import { AuthenticationModule } from 'src/authentication/authentication.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MovieRepository]),
     TagsModule,
     UsersModule,
-    AuthenticationModule,
   ],
   providers: [MoviesService],
   controllers: [MoviesController],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, UsersModule],
 })
 export class MoviesModule {}
