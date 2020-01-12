23c1abbd30eab6ba31357fd85a8a1b40a9d83845
Antonio Mata <hildeamp@gmail.com>
2020-01-11 20:25:45 -0600

    :white_check_mark: create test module

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,12 +1,29 @@
+/*eslint-disable */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { UserRepository } from '../users/repositories/user.repository';
+import { MovieRepository } from '../movies/repositories/movie.repository';
 
 describe('OrdersService', () => {
   let service: OrdersService;
 
+  const mockUserRepo = () => ({});
+
+  const mockOrderRepo = () => ({});
+
+  const mockMovieRepo = () => ({});
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        { provide: UserRepository, useFactory: mockUserRepo },
+        { provide: MovieRepository, useFactory: mockMovieRepo },
+        { provide: getRepositoryToken(Order), useFactory: mockOrderRepo },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
